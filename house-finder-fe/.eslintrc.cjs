55cc0792c4cda4f023584c21ae17aa08c6bf49ac
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2024-03-25 23:54:12 +0100

    update lint file

--- house-finder-fe/.eslintrc.cjs
+++ house-finder-fe/.eslintrc.cjs
@@ -9,6 +9,9 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    // TODO: Remove this rule
+    '@typescript-eslint/no-explicit-any': 'off'
   },
 }
